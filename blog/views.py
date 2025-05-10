79eb639d67bf1b5ecfded71131328445d959ee18
kelvin udufo <kudufo@gmail.com>
2025-05-11 00:39:45 +0100

    corrected the spelling of my title and also registered blog post for the admin, so it would be possible to update and delete blog post and change authors

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        'author': 'kelvin',
-        'title': 'blog post 1',
-        'date_posted': 'August 27,2017',
-         'content': 'first post content'
-    },
-     {
-        'author': 'jude',
-        'title': 'blog post 2',
-        'date_posted': 'August 29,2017',
-        'content': 'second post content'
-    }
-]
-
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
