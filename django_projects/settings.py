e3f10e9ed8a0beb1fa448603ded7f2781ae424aa
kelvin udufo <kudufo@gmail.com>
2025-05-20 21:29:14 +0100

    added a crispy module to beautify the register page

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    "crispy_forms",
+    "crispy_bootstrap4",
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
